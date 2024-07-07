c59fa8a629905aa89a8b6913f54a80991eb27a2b
Mirna Medeiros <mirna.medeiros.053@imd.ufrm.br>
2024-07-07 09:41:06 -0300

    Dinamic Login Page

--- src/main/java/pds/comasy/controller/IndexController.java
+++ src/main/java/pds/comasy/controller/IndexController.java
@@ -1,5 +1,6 @@
 package pds.comasy.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +10,14 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/")
 public class IndexController {
 
+    @Value("${systemType}")
+    private String systemType;
+
     @GetMapping(value = "/login")
     public ModelAndView login() {
-        return new ModelAndView("login");
+        ModelAndView model = new ModelAndView("login");
+        model.addObject("systemType", systemType);
+        return model;
     }
 
     @GetMapping(value = "/logout")
